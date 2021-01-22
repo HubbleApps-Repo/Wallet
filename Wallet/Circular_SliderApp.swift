ba19c3a40554764d1481319f5a498eeafa4e0ab7
Denzel Banegas <denzelbanegas@gmail.com>
2021-01-22 16:24:26 +0000

    Version 1.0

---
+++ Wallet/Circular_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circular_SliderApp.swift
+//  Wallet
+//
+//  Created by denzel banegas on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Circular_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Wallet/ContentView.swift
+++ Wallet/ContentView.swift
@@ -6,75 +6,104 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        
+        NavigationView{
+            
+            Home()
+                .preferredColorScheme(.dark)
+                .navigationTitle("Wallet")
+                
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+    }
+}
+
+struct Home : View {
+    
+    @State var size = UIScreen.main.bounds.width - 100
+    @State var progress : CGFloat = 0
+    @State var angle : Double = 0
+    
+    var body: some View{
+        
+        VStack{
+            
+            ZStack{
+                
+                Circle()
+                    .stroke(Color("stroke"),style: StrokeStyle(lineWidth: 55, lineCap: .round, lineJoin: .round))
+                    .frame(width: size, height: size)
+                
+                // progress....
+                
+                Circle()
+                    .trim(from: 0, to: progress)
+                    .stroke(Color.pink,style: StrokeStyle(lineWidth: 55, lineCap: .butt))
+                    .frame(width: size, height: size)
+                    .rotationEffect(.init(degrees: -90))
+                
+                // Inner Finish Curve...
+                
+                Circle()
+                    .fill(Color("stroke"))
+                    .frame(width: 55, height: 55)
+                    .offset(x: size / 2)
+                    .rotationEffect(.init(degrees: -90))
+                
+                // Drag Circle...
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 55, height: 55)
+                    .offset(x: size / 2)
+                    .rotationEffect(.init(degrees: angle))
+                // adding gesture...
+                    .gesture(DragGesture().onChanged(onDrag(value:)))
+                    .rotationEffect(.init(degrees: -90))
+                
+                // sample $200
+                Text("$" + String(format: "%.0f", progress * 200))
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+            }
+        }
+    }
+    
+    func onDrag(value: DragGesture.Value){
+        
+        // calculating radians...
+        
+        let vector = CGVector(dx: value.location.x, dy: value.location.y)
+        
+        // since atan2 will give from -180 to 180...
+        // eliminating drag gesture size
+        // size = 55 => Radius = 27.5...
+        
+        let radians = atan2(vector.dy - 27.5, vector.dx - 27.5)
+        
+        // converting to angle...
+        
+        var angle = radians * 180 / .pi
+        
+        // simple technique for 0 to 360...
+        
+        // eg = 360 - 176 = 184...
+        if angle < 0{angle = 360 + angle}
+        
+        withAnimation(Animation.linear(duration: 0.15)){
+            
+            // progress...
+            let progress = angle / 360
+            self.progress = progress
+            self.angle = Double(angle)
+        }
     }
 }
