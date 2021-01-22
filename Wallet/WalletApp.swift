51274b6d8cc9680e603e37f1a8ab13eb2d600813
Denzel Banegas <denzelbanegas@gmail.com>
2021-01-22 16:15:21 +0000

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,20 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by denzel banegas on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
